026be6b8d37fab39ffbe57b8e19a7206e7a3bec0
lcswsher <lcswisher@yahoo.com>
2021-08-04 13:04:07 -0500

    updates to the logic JS file for access to the geoJSON file

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,39 +1,42 @@
-// Use this link to get the GeoJSON data.
-const link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+// Use this link to get the GeoJSON data (all week).
 
-// var myMap = L.map("map", {
-//     center: [40.7128, -74.0059],
-//     zoom: 11
-// });
+const link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// initial 
+var myMap = L.map("map", {
+    center: [39.0119, -98.4842],
+    zoom: 5
+});
+
+// check the console log to ensure Json file has loaded
 
 d3.json(link).then(function (data) {
-    createFeatures(data.features);
+   createFeatures(data.features);
+   console.log(data.features)
 });
 
 function createFeatures(earthquakeData) {
 
+    // Define a function that we want to run once for each feature in the features array.
+    // Give each feature a popup that describes the place and time of the earthquake.
+    function onEachFeature(feature, layer) {
+       layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+    }
 
-
-
-
-
-
-
-
-    
+    // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+    // Run the onEachFeature function once for each piece of data in the array.
+    var earthquakes = L.geoJSON(earthquakeData, {
+       onEachFeature: onEachFeature
+    });
+    createMap(earthquakes);
 }
 
-// {L.geoJson(data).addTo(myMap);});
-
-
-
-// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-// }).addTo(myMap);
-
-
-
-
-// console.log()
+function createMap(earthquakes) {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(myMap);
+    console.log()
+}
 
 
